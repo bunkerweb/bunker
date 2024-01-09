1d3b642f8427e61ed8ab06e0b7b73a97e311cccd
proudparrot2 <hi@proudparrot2.tech>
2024-01-09 11:11:17 -0600

    remove box icons

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,7 +20,7 @@ module.exports = {
         primary: {
           DEFAULT: 'hsl(var(--primary))',
           foreground: 'hsl(var(--primary-foreground))'
-        },
+        }, 
         secondary: {
           DEFAULT: 'hsl(var(--secondary))',
           foreground: 'hsl(var(--secondary-foreground))'
