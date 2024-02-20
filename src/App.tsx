ead047859ff67a0a204281f7c45b8337a780bb95
proudparrot2 <hi@proudparrot2.tech>
2024-02-20 10:01:12 -0600

    auto about:blank

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,6 @@ window.document.documentElement.classList.add('dark')
 export default function App() {
   useEffect(() => {
     registerDefaultPlugins()
-    // handleNewPlugins()
   }, [])
 
   return (
