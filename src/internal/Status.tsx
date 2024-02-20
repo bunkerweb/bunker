de86384d77e0eb8b40a7b3693fa42f875f9a7e2c
proudparrot2 <hi@proudparrot2.tech>
2024-02-20 10:00:08 -0600

    start changelog

--- src/internal/Status.tsx
+++ src/internal/Status.tsx
@@ -1,11 +1,24 @@
 import { Plugin } from '@/lib/pluginloader'
 
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from '@/components/ui/dialog'
+import { useEffect, useState } from 'react'
+
 const Status: Plugin = {
   name: 'Status',
   id: 'bunker.status',
   description: 'Display some important internal information about Bunker',
 
   tile() {
+    const [changelogOpen, setChangelogOpen] = useState(false)
+    useEffect(() => {
+      const changelog = setTimeout(() => {
+        setChangelogOpen(true)
+      }, 3000)
+
+      return () => {
+        clearTimeout(changelog)
+      }
+    }, [])
     return (
       <div>
         <p>
@@ -17,12 +30,21 @@ const Status: Plugin = {
         <p>
           Version: <span className="text-orange-500 font-semibold">0.1 (dev)</span>
         </p>
+
+        <Dialog open={changelogOpen} onOpenChange={setChangelogOpen}>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Changelog</DialogTitle>
+              <DialogDescription>Bunker got a brand new update. You're going to love it.</DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
       </div>
     )
   },
-  
+
   onReady() {
-    console.log("ready")
+    console.log('Status')
   }
 }
 
