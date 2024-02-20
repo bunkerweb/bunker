32b69d6181ceafb1848dea658a4de6e3f0e9304d
proudparrot2 <hi@proudparrot2.tech>
2024-02-20 09:59:24 -0600

    formatting

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
-import million from 'million/compiler';
+import million from 'million/compiler'
 import path from 'path'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [million.vite({ auto: true, mute: true  }), react()],
+  plugins: [million.vite({ auto: true, mute: true }), react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
     }
   }
 })
- 
