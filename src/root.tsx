1d9e6645301096adcfb8a14fd654217a05018711
proudparrot2 <hi@proudparrot2.tech>
2024-03-07 08:28:15 -0600

    mask about:blank tab to google classroom

--- src/root.tsx
+++ src/root.tsx
@@ -13,16 +13,23 @@ window.document.documentElement.classList.add('dark')
 
 if (!window.frameElement) {
   const tab = window.open('about:blank', '_blank')
-  const iframe = tab!.document.createElement('iframe')
-  const stl = iframe!.style
-  stl.border = stl.outline = 'none'
-  stl.width = '100vw'
-  stl.height = '100vh'
-  stl.position = 'fixed'
-  stl.left = stl.right = stl.top = stl.bottom = '0'
-  iframe!.src = self.location.href
-  tab!.document.body.appendChild(iframe!)
-  window.location.replace('https://classroom.google.com')
+  
+    const iframe = tab!.document.createElement('iframe')
+    const stl = iframe!.style
+    stl.border = stl.outline = 'none'
+    stl.width = '100vw'
+    stl.height = '100vh'
+    stl.position = 'fixed'
+    stl.left = stl.right = stl.top = stl.bottom = '0'
+    iframe!.src = self.location.href
+    tab!.document.body.appendChild(iframe!)
+
+    tab!.document.title = 'Home'
+    const favicon = tab!.document.createElement("link")
+    favicon.href = "https://ssl.gstatic.com/classroom/ic_product_classroom_32.png"
+    favicon.rel = "shortcut icon"
+    tab!.document.head.appendChild(favicon)
+    window.location.replace('https://classroom.google.com')
   
 }
 
