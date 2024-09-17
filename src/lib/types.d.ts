6e244952d72747eb2b3a0937265e1f643d7193be
proudparrot2 <71939973+proudparrot2@users.noreply.github.com>
2024-09-17 14:47:20 -0500

    Add comments to the Plugin properties

--- src/lib/types.d.ts
+++ src/lib/types.d.ts
@@ -1,17 +1,46 @@
+import { LucideIcon } from "lucide-react"
 import { SDK } from "./sdk"
 
 export interface Plugin {
+  /** The name of the plugin to be displayed. */
   name: string
+
+  /** A unique plugin identifier. */
   id: string
+
+  /**
+    If the plugin should be disabled.
+    (Mostly only used internally)
+  */
   disabled?: boolean
+
+  /** A description of the plugin to be displayed. */
   description?: string
+
+  /**
+   An icon that represents the plugin to be displayed in the sidebar.
+   (Only shows in the sidebar if `page()` is used)
+   */
   icon?: LucideIcon
+
+  /**
+    The URL for the plugin source.
+    (Mostly only used internally)
+  */
   source?: string
 
   navPosition?: "top" | "bottom"
+
+  /**
+    Render a tile on the homescreen using a React component.
+    Returned components will be displayed in the tile under the plugin's name.
+  */
   tile?: (sdk: SDK) => React.ReactNode
+  
+  /** Render a page to be accessed from the sidebar using a React component. */
   page?: (sdk: SDK) => React.ReactNode
 
+  /** Event that fires when the plugin is first loaded. */
   onReady?: (sdk: SDK) => void
 }
 
